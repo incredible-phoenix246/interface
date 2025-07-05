ead64c9ec66085e11e6f8f9c0d3c997e901ba5fd
CeoFred <johnsonmessilo19@gmail.com>
2025-07-05 12:37:02 +0100

    feat(scope): enhance profile page with wallet connection handling

--- pages/profile.page.tsx
+++ pages/profile.page.tsx
@@ -1,8 +1,32 @@
+import { Box } from '@mui/material';
 import { MainLayout } from 'src/layouts/MainLayout';
 import ProfilePage from 'src/modules/profile/profile-page';
 
+import { ConnectWalletPaper } from '../src/components/ConnectWalletPaper';
+import { ContentContainer } from '../src/components/ContentContainer';
+import { useWeb3Context } from '../src/libs/hooks/useWeb3Context';
+
 export default function ProfilePageInterface() {
-  return <ProfilePage />;
+  const { currentAccount, loading: web3Loading } = useWeb3Context();
+
+  return (
+    <ContentContainer>
+      {currentAccount ? (
+        <ProfilePage />
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            mt: { xs: 3, xsm: 4, lg: 5 },
+            height: '100vh',
+          }}
+        >
+          <ConnectWalletPaper loading={web3Loading} />
+        </Box>
+      )}
+    </ContentContainer>
+  );
 }
 
 ProfilePageInterface.getLayout = function getLayout(page: React.ReactElement) {
